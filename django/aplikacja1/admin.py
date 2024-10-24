dbb3acf4dbdf26df48a939b1b01f66f2ea4edf11
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2024-10-24 12:35:28 +0200

    Delete django directory

--- django/aplikacja1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Czlowiek
-
-admin.site.register(Czlowiek)
-# Register your models here.
--- django/aplikacja1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Czlowiek
-
-class CzlowiekForm(forms.ModelForm):
-    class Meta:
-        model = Czlowiek
-        fields = '__all__'
