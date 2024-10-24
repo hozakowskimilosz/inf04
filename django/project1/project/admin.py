ce493dcf4d44e61cbfe373c155f7cfdb2de26a27
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2024-10-24 11:30:16 +0200

    Delete django/project1 directory

--- django/project1/project/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Czlowiek
-
-# Register your models here.
-admin.site.register(Czlowiek)
--- django/project1/project/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Czlowiek(models.Model):
-    imie = models.CharField(max_length=30, unique=False, blank=False, verbose_name='imie')
