194619638872573f1f27657ed04f59659b9be366
Milossh_ <89982638+hozakowskimilosz@users.noreply.github.com>
2024-10-25 13:42:42 +0200

    Delete django directory

--- django/aplikacja1/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import Group, User
-from rest_framework import serializers
-from .models import Czlowiek
-
-class CzlowiekSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Czlowiek
-        fields = ['imie']
